c15734de9a485eab9fee99533e40d2865dbf1d26
PJXIO-Sabbahi <142024439+PJXIO-Dev@users.noreply.github.com>
2025-10-21 12:07:41 +0300

    first commit

---
+++ Project_1_object_detection_traffic_light.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Project 1: Object Detection on Traffic Light Dataset
+# 
+# In this project, the goal is to train an object detection model to detect traffic light color using the dataset provided in this notebook. You can use any framework or model, for example YOLO or RT-DETR and inference techniques like SAHI for better detection of small objects. The focus is on creating an accurate and efficient solution for identifying traffic light color.
+# 
+# **The dataset is provided in YOLO format.**
+# However, you are free to convert it to any format of your choice according to the library that you are using if this will help you to get mAP50-95 >= 48% for the sake of training the model**
+# 
+# **The dataset contains a total of 5 classes:**
+# 
+# ```python
+# 0: 'Green',
+# 1: 'off',
+# 2: 'red',
+# 3: 'wait_on',
+# 4: 'yellow'  
+# ```
+# 
+# ## Your Tasks
+# - Train an object detection model.
+# - Perform inference on the given video file.
+# - Provide a video or writeup link explaining your code and approach.
+# - Give a detailed written description or a video/screen recording explaining your approach and code.
+# 
+
+# <h2 style = "color: green">1. Visualize dataset [Points 15]</h2>
+# 
+# 
+# **In this sub-section, you have to plot a few images with their bounding box labels overlayed similar to one shown below. you have the dataset and training folder**
+# 
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# <h2 style = "color: green;">2. Train the Model </h2>
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# <h2 style = "color: green;">3. Validate the Model </h2>
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# <h2 style = "color: green;">4. Inference on given video [10 Points]</h2>
+# 
+# 
+# 
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# <h2 style = "color: green;">5. Writeup/Video Explanation [5 Points]</h2>
+
+# In[ ]:
+
+
+
+
